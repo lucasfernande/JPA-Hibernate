6e4304661ae257b37f6172c1625038103440996e
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-12 16:10:54 -0300

    Buscando uma pessoa por Id

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,19 +10,15 @@ public class Programa {
 
 	public static void main(String[] args) {
 	 
-		Pessoa p1 = new Pessoa(null, "Mano Brownie", "maninho@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Maria da Silva", "maria@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Carlos Júnior", "carlos@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin(); // iniciando a transação		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-        em.getTransaction().commit(); // finalizando e confirmando a transação
+		
+		Pessoa p = em.find(Pessoa.class, 2);
         
+		System.out.println(p);
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
 	}
 
 }
