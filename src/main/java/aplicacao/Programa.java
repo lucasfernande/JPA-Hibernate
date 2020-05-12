3ec88afdc680813d20b4bcbd2107d47eb7288f28
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-12 16:06:11 -0300

    Inserindo dados no phpMyAdmin

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,19 +1,28 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 	 
-		Pessoa p1 = new Pessoa(1, "Mano Brownie", "maninho@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Maria da Silva", "maria@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Carlos Júnior", "carlos@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Mano Brownie", "maninho@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Maria da Silva", "maria@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Carlos Júnior", "carlos@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin(); // iniciando a transação		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+        em.getTransaction().commit(); // finalizando e confirmando a transação
+        
+        System.out.println("Pronto!");
 	}
 
 }
